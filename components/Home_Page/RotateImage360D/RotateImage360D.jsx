58276d7e140c309249961977331047d0450e054b
vishnu kumawat <vishnu.kumawat@decipherzone.com>
2021-04-16 17:34:44 +0530

    Add env files

--- components/Home_Page/RotateImage360D/RotateImage360D.jsx
+++ components/Home_Page/RotateImage360D/RotateImage360D.jsx
@@ -9,12 +9,13 @@ import Rotate360DStyle from './RotateImage360D.module.css';
 **/
 
 const RotateImage360D = (props) => {
+  
   return(
     <section className={Rotate360DStyle.section_outer}>
       <Container>
         <Row>
           <Col lg={3}>
-            {/* <ScrollAnimation duration={2} animateIn="fadeInLeft" animateOnce={true}> */}
+            <ScrollAnimation duration={2} animateIn="fadeInLeft" animateOnce={true}>
               <div className={Rotate360DStyle.iconbox}>
                 <img alt="Rotating Deck" src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/Marra-Forni-Rotating-Deck-icon.webp`} width="68" height="68" />
                 <h5>Rotating Deck</h5>
@@ -25,22 +26,22 @@ const RotateImage360D = (props) => {
                 <h5>Plug & Play Ventilation</h5>
                 <p>Compressed Wool Insulation, Double Wall Venting and 103 and 1978 UL listed integrated venting</p>
               </div>
-            {/* </ScrollAnimation> */}
+            </ScrollAnimation>
           </Col>
           <Col lg={6}>
-            {/* <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={true}> */}
+            <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={true}>
               <Tabs defaultActiveKey="RedPennyTile" id="uncontrolled-tab-example">
                 <Tab eventKey="RedPennyTile" title="Red Penny Tile">
-                  
+                Red Penny Tile
                 </Tab>
                 <Tab eventKey="RedBrokenTile" title="Red Broken Tile">
-                  
+                Red Broken Tile
                 </Tab>
               </Tabs>
-            {/* </ScrollAnimation> */}
+            </ScrollAnimation>
           </Col>
           <Col lg={3}>
-            {/* <ScrollAnimation duration={2} animateIn="fadeInRight" animateOnce={true}> */}
+            <ScrollAnimation duration={2} animateIn="fadeInRight" animateOnce={true}>
               <div className={Rotate360DStyle.iconbox}>
                 <img alt="TouchScreen" src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/Marra-Forni-Plug-and-Play-Ventilation-icon.webp`} width="68" height="68" />
                 <h5>TouchScreen</h5>
@@ -51,7 +52,7 @@ const RotateImage360D = (props) => {
                 <h5>84,000 BTU</h5>
                 <p>Lower energy bills enable you to invest in the grow of you business</p>
               </div>
-            {/* </ScrollAnimation> */}
+            </ScrollAnimation>
           </Col>
         </Row>
       </Container>
