e2b26829970e744df9551c95e045a509dc37747d
vishnu kumawat <vishnu.kumawat@decipherzone.com>
2021-04-29 20:47:36 +0530

    Tools and accessories Page Design 50% completed

---
+++ components/ToolsAndAccessories_Page/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from '../header/header'
+import Footer from '../footer/footer'
+import GetAQuote from '../common/GetAQuote/GetAQuote'
+import T_and_A_Style from './index.module.css'
+import { Container } from 'react-bootstrap'
+import Products from './T&C_Products/Products'
+
+const ToolsAndAccessoriesMain = (props) => {
+  return(
+    <>
+      <Header />
+      <div style={{
+        height:'610px',
+        backgroundImage:`url(${process.env.NEXT_PUBLIC_BASE_PATH}/images/Marra-Tools-Accessories-Banner.jpg)`,
+        backgroundRepeat:'no-repeat',
+        backgroundAttachment:'fixed',
+        backgroundSize:'cover',
+        backgroundPosition: 'center 0'
+      }}/>
+      <Container>
+        <div className={T_and_A_Style.heading_main}>
+          <h3>The Right Tool For The Right Recipe</h3>
+          <p>The perfect pizza is the result of the right ingredients, the right oven, and the right tools and accessories to get the job done!  All of Marra Forni’s quality brick oven cooking tools and accessories are designed to meet the specific needs and demands of our customers; from line cook to the chef, to artisan pizza maker; with durability, efficiency, and simplicity mind. Remember to think outside of the traditional “pizza box” and expand the bandwidth of your menu with the right tools and accessories.</p>
+          <p>Complete the form below to get pricing information in minutes.</p>
+        </div>
+      </Container>
+      <Products />
+      <GetAQuote />
+      <Footer />
+    </>
+   )
+
+ }
+
+export default ToolsAndAccessoriesMain
