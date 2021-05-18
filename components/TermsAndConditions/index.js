62e802e6334dd9ebde9bf869fa3126bf0d4003ed
vishnu kumawat <vishnu.kumawat@decipherzone.com>
2021-05-18 18:57:16 +0530

    Create privacy policy and terms & conditions pages

---
+++ components/TermsAndConditions/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../header/header'
+import Footer from '../footer/footer'
+import TermsAndConditionsContent from './TermsAndConditionsContent/TermsAndConditionsContent'
+
+const TermsAndConditionsMain = (props) => {
+  return(
+    <>
+      <Header />
+      <TermsAndConditionsContent />
+      <Footer />
+    </>
+   )
+
+ }
+
+export default TermsAndConditionsMain
---
+++ pages/privacy-policy/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from '../../components/common/Head'
+import PrivacyPolicyMain from '../../components/PrivacyPolicy'
+
+const PrivacyPolicy = (props) => {
+  return(
+    <>
+      <Head
+        title='Privacy Policy - MarraForni'
+        description='MARRA FORNI PRIVACY POLICY'
+      />
+      <PrivacyPolicyMain />
+    </>
+   )
+
+ }
+
+export default PrivacyPolicy
