d9211ce2a3ba25a65c87804bd6180abcb0b54ff8
Sunil Suthar <sunil.suthar@decipherzone.com>
2021-07-22 12:52:55 +0530

    Fixed slider numbers of sildes

--- components/Press_Page/VideoSlider/VideoSlider.jsx
+++ components/Press_Page/VideoSlider/VideoSlider.jsx
@@ -9,20 +9,21 @@ import Link from "next/link";
 const VideoSlider = (props) => {
     const pressData = props.data
     const AllPressData = pressData && pressData.content && pressData.content.length ? pressData.content : []
+    console.log(AllPressData.slice(0,4))
 
     let VideoSliderSettings = {
         speed: 1500,
         arrows: false,
         dots: false,
-        slidesToShow: 7,
-        slidesToScroll: 7,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         autoplay: true,
         infinite: true,
         responsive: [
             {
                 breakpoint: 1400,
                 settings: {
-                    slidesToShow: 5
+                    slidesToShow: 4
                 }
             },
             {
@@ -58,8 +59,9 @@ const VideoSlider = (props) => {
                 <Col lg={10}>
                     <h2>Videos</h2>
                     <Slider {...VideoSliderSettings}>
-                        {(AllPressData && AllPressData.length) ? AllPressData.map((data, i) => (
-                            <div key={data.id}>
+                        {(AllPressData && AllPressData.length) ? AllPressData.map((data, i) => {
+                            debugger
+                          return ( <div key={data.id}>
                                 <div className={VideoSliderStyle.slider_inner_main}>
                                     <Link href={data.videoUrl}>
                                         <a title={data.title}>
@@ -83,8 +85,8 @@ const VideoSlider = (props) => {
                                         </a>
                                     </Link>
                                 </div>
-                            </div>
-                        )) : null}
+                            </div>)
+                        }) : null}
                         {/*<div>*/}
                         {/*  <div className={VideoSliderStyle.slider_inner_main}>*/}
                         {/*    <div className={VideoSliderStyle.slider_inner}>*/}
