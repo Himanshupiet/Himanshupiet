04d00c7b8520385f9313813c1cf991010612d182
Priyanka Sheoran <priyanka.sheoran@decipherzone.com>
2021-07-30 17:30:49 +0530

    add map functionlity in map

--- components/common/Map/Map.js
+++ components/common/Map/Map.js
@@ -9,6 +9,10 @@ import {
     InfoWindow
 } from 'react-google-maps'
 
+import { Col, Row, Container  } from 'react-bootstrap'
+
+import GoogleSuggest from './LocationSearch'
+
 import { API_HOST } from '../../../env'
 import axios from 'axios'
 import mapStyle from './Map.module.css'
@@ -17,36 +21,84 @@ import mapStyle from './Map.module.css'
 
 const MapWithAMakredInfoWindow = compose(
     withStateHandlers(() => ({
-        isOpen: false,
     }), {
-        onToggleOpen: ({ isOpen }) => () => ({
-            isOpen: !isOpen,
-        })
+
     }),
     withScriptjs,
     withGoogleMap
  )(props => {
+    const [ mapLists, setMapLists ] = useState([])
+
+    useEffect(() => {
+        setMapLists(props.mapList)
+    },[props.mapList])
+
+    const onToggleOpen = (item, loc, v ) => {
+        let listArr = mapLists.map(type => {
+            if(type.type == v.type) {
+                return {
+                    ...type,
+                    locationDetailList:type.locationDetailList.map(c => {
+                     if(c.location == loc.location){
+                         return{
+                             ...c,
+                             addressDetailList:c && c.addressDetailList && c.addressDetailList.length && c.addressDetailList.map(cor => {
+                                 if(cor.companyName == item.companyName){
+                                   return{
+                                       ...cor,
+                                       isOpen:!cor.isOpen
+                                   }
+                                 }else{
+                                     return{
+                                         ...cor
+                                     }
+                                 }
+                             })
+                         }
+
+                     }else{
+                         return{
+                             ...c
+                         }
+                     }
+                   })
+                }
+            }else{
+                return{
+                    ...type
+                }
+            }
+
+        })
+        setMapLists(listArr)
+    }
+
     return(
             <GoogleMap
-                defaultZoom={2}
+                defaultZoom={1}
                 defaultCenter={{lat: 26.874760, lng: 75.759070}}
             >
-                { props &&
-                  props.mapList &&
-                  props.mapList.length ?
-                    props.mapList.map(v => (
-                        v && v.locationDetailList && v.locationDetailList.length && v.locationDetailList.map(loc => (
-                            loc && loc.addressDetailList && loc.addressDetailList.map((item, i) => {
-                                
+                { mapLists &&
+                   mapLists.length ?
+                    mapLists.map(v => (
+                        v &&
+                        v.locationDetailList &&
+                        v.locationDetailList.length &&
+                        v.locationDetailList.map(loc => (
+                            loc &&
+                            loc.addressDetailList &&
+                            loc.addressDetailList.map((item, i) => {
                             return (
                                 <Marker
-                                    position={{lat:item.latitude , lng: item.longitude}}
-                                    onClick={props.onToggleOpen}
+                                    position={{
+                                        lat:item.latitude ,
+                                        lng: item.longitude
+                                    }}
+                                    onClick={() => onToggleOpen(item, loc, v)}
                                     key={i}
                                 >
-                                    {console.log(v.type, 'kkkk')}
-                                    { props.isOpen &&
-                                        <InfoWindow onCloseClick={props.onToggleOpen}>
+                                    { item.isOpen &&
+                                        <InfoWindow onCloseClick={() => onToggleOpen(item, loc, v)}>
                                             <div className={mapStyle.info_box_outer}>
                                                 <p>
                                                     <strong>
@@ -56,7 +108,7 @@ const MapWithAMakredInfoWindow = compose(
                                                         : (v.type == 'dealer') 
                                                         ? 'Dealer' 
                                                         : 'Service Agent'
-                                                    } {v.type}
+                                                    }
                                                     </strong>
                                                 </p>
                                                 <p>
@@ -82,7 +134,9 @@ const MapWithAMakredInfoWindow = compose(
 );
 
 const ReactGoogleMaps = (props) => {
-    const [ map, setMap ] = useState([])
+    const [ mapList, setMapList ] = useState([])
+    const [ list, setList ] = useState([])
+    const [ serviceType, setServiceType ] = useState('All')
 
     useEffect(() => {
         axios({
@@ -94,21 +148,103 @@ const ReactGoogleMaps = (props) => {
             method: 'get'
         })
             .then(response => {
-                setMap(response.data)
+                let locArr = response.data.map(v => {
+                    return {
+                        ...v,
+                        locationDetailList: v &&
+                            v.locationDetailList &&
+                            v.locationDetailList.length &&
+                            v.locationDetailList.map(loc => {
+                                    return {
+                                        ...loc,
+                                        addressDetailList: loc &&
+                                            loc.addressDetailList &&
+                                            loc.addressDetailList.map((item, i) => {
+                                                return {
+                                                    ...item,
+                                                    isOpen: false
+                                                }
+                                            })
+                                    }
+                                }
+                            )
+                        }
+                   }
+                )
+                setMapList(locArr)
+                setList(locArr)
             })
             .catch(error => {
                 return null
             })
     },[])
 
+    useEffect(() => {
+        if (serviceType == 'All') {
+            setMapList(list)
+        } else {
+          let filterMapList = list.filter(val => val.type == serviceType)
+          setMapList(filterMapList)
+        }
+    },[serviceType])
+
     return(
-        <MapWithAMakredInfoWindow
-            mapList={ map || [] }
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD16lyRRADgCEM0WxceqKr68bq_THANws&v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{height: `100%`}}/>}
-            containerElement={<div style={{height: `400px`}}/>}
-            mapElement={<div style={{height: `100%`}}/>}
-        />
+        <Container className={'mt-5'}>
+            <Row>
+                <Col md={4}>
+                    <label>Search By Address</label>
+                    <GoogleSuggest/>
+                </Col>
+                <Col md={4}>
+                    <label>View Map By Service Type</label>
+                    <select onChange={(e) => setServiceType(e.target.value)}>
+                        <option value={ 'All' }>All Service Types</option>
+                        { list.map((val, i) => {
+                            return(
+                                <>
+                                   <option value={ val.type } key={i}>
+                                       { val.type }
+                                   </option>
+                                </>
+                             )
+                           })
+                        }
+                    </select>
+                </Col>
+                <Col md={4}>
+                    <Row className={'mt-4'} style={{
+                        textAlign: 'center'
+                    }}>
+                        {
+                            list.map((val, i) => {
+                                return(
+                                    <Col md={4} key={i} style={{fontSize:'18px', display: 'grid'}}>
+                                        <i className={'fa fa-map-marker '}
+                                           style={{color:
+                                               val.type === 'rep' ?
+                                               '#00A300':
+                                                val.type === 'dealer' ?
+                                                '#1e7ba6':
+                                                '#e31422',
+                                                 fontSize: '26px'
+                                           }}
+                                        />
+                                        {val.type}
+                                    </Col>
+                                )
+                            })
+                        }
+                    </Row>
+                </Col>
+            </Row>
+            <MapWithAMakredInfoWindow
+                mapList={ mapList || [] }
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD16lyRRADgCEM0WxceqKr68bq_THANws&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{height: `100%`}}/>}
+                containerElement={<div style={{height: `500px`}}/>}
+                mapElement={<div style={{height: `100%`}}/>}
+            />
+        </Container>
     )
 };
 
--- env.js
+++ env.js
@@ -1,3 +1,5 @@
 const API_HOST = 'https://jdev.decipherzone.com/mfback/'
+const MAP_API_KEY = 'AIzaSyCD16lyRRADgCEM0WxceqKr68bq_THANws'
 
 exports.API_HOST = API_HOST
+exports.MAP_API_KEY = MAP_API_KEY
