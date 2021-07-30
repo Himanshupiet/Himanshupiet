3285bace818d0df21262f9e1ee5c23fdc91a9088
Priyanka Sheoran <priyanka.sheoran@decipherzone.com>
2021-07-30 11:32:07 +0530

    set fun in map

--- components/common/Map.js
+++ components/common/Map.js
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { compose, withStateHandlers } from 'recompose'
 import {
     withScriptjs,
@@ -21,29 +21,41 @@ const MapWithAMakredInfoWindow = compose(
     }),
     withScriptjs,
     withGoogleMap
-)(props =>
-    <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    >
-        <Marker
-            position={{ lat: -34.397, lng: 150.644 }}
-            onClick={props.onToggleOpen}
-        >
-            {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-                <i className={'fa fa-user'} />
-            </InfoWindow>}
-        </Marker>
-    </GoogleMap>
+ )(props => {
+    return(
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{lat: -34.397, lng: 150.644}}
+            >
+                <Marker
+                    position={{lat: -34.397, lng: 150.644}}
+                    onClick={props.onToggleOpen}
+                >
+                    {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+                        <i className={'fa fa-user'}/>
+                    </InfoWindow>}
+                </Marker>
+            </GoogleMap>
+    )
+    }
 );
 
-const ReactGoogleMaps = () => [
-    <MapWithAMakredInfoWindow
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD16lyRRADgCEM0WxceqKr68bq_THANws&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-    />
-];
+const ReactGoogleMaps = (props) => {
+    const [ map, setMap ] = useState([{name:'fff'}])
+
+    useEffect(() => {
+
+    },[])
+
+    return(
+        <MapWithAMakredInfoWindow
+            map={ map }
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD16lyRRADgCEM0WxceqKr68bq_THANws&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{height: `100%`}}/>}
+            containerElement={<div style={{height: `400px`}}/>}
+            mapElement={<div style={{height: `100%`}}/>}
+        />
+    )
+};
 
 export default enhance(ReactGoogleMaps);
