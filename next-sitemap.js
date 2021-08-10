cdb09a587971eee7c7102bf239401d5fb956938b
Priyanka Sheoran <priyanka.sheoran@decipherzone.com>
2021-08-10 16:09:50 +0530

    add sitemap file

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://example.com',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://example.com/server-sitemap.xml', // <==== Add here
+    ],
+  },
+}
